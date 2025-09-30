a2d8090b2b5a3e59c20538fcc4936858994d4e44
Kaviya-Shalini <shalinikaviya5@gmail.com>
2025-09-30 22:41:27 +0530

    update commit

--- NextgenBnp/src/main/java/com/example/NextgenBnp/config/WebConfig.java
+++ NextgenBnp/src/main/java/com/example/NextgenBnp/config/WebConfig.java
@@ -1,21 +1,18 @@
 package com.example.NextgenBnp.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("OPTIONS","GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("OPTIONS","GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
